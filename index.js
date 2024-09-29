18700e646c57d0dbc154e724e9c2a3b3ab164b4d
sharonodongo <sharon94odongo@gmail.com>
2024-09-29 18:13:57 +0300

    submitting assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
